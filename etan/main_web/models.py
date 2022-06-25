85224c0b78478c1bf1d4374f4bb0bde306bd671c
Ivan <d1034182016@gmail.com>
2022-06-25 14:43:11 +0800

    v1. before pull github

--- etan/main_web/models.py
+++ etan/main_web/models.py
@@ -1,3 +1,28 @@
+from statistics import mode
 from django.db import models
 
 # Create your models here.
+
+class taipei (mode.Model):
+    area = models.CharField(max_length=20)
+    layer = models.CharField(max_length=20)
+    build_type = models.CharField(max_length=20)
+  
+
+    class Meta:
+        db_table = "aig_counter_types"
+
+    def __str__(self):
+        return self.area
+
+        
+class city (models.Model):
+
+    id = models.IntegerField(primary_key=True)
+    text = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = "aig_counter_types"
+
+    def __str__(self):
+        return self.text
